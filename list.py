bd535b3a62e34c1516455c18eacd318b032f7d90
Aditya Chaudhary <adityachaudhary399@gmail.com>
2025-07-02 17:45:13 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,28 @@
+lst=['Aditya','Chaudhary','Aman','Nishu']
+print(lst)
+
+
+# Acessing Element through Index
+print(lst[3])
+print(len(lst))     # length of list
+print("*"*50)
+
+lst[2]='Sakshi'
+print(lst)
+
+# Slice List
+print(lst[0:3])
+
+# Reverse the List
+print(lst[::-1])
+
+adi=[1,3,67,89,12,6,8,0,7,9]
+print(adi)
+print("*"*50)
+for i in adi:
+    print(i)
+    
+print("*"*50)
+
+for i in range(len(adi)):
+    print(adi[i])
